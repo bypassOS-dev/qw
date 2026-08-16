515dd7cb4723f456c3161f5760f762ac901a73d5
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-16 12:10:22 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use tracing::info;
+use tracing::Instrument;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
-    
-    let span = tracing::info_span!("client", id = 1);
-    let _guard = span.enter();
-    info!("Hi");
+    let task = handle_client(1);
+    let task1 = handle_client(2);
+
+    tokio::join!(task, task1);
+    println!("All task is created!");
+}
+async fn handle_client(id: i32) {
+    let span = tracing::info_span!("client", id = id);
+    async {
+        tracing::info!("Client connected!");
+        tokio::time::sleep(tokio::time::Duration::from_millis(123)).await;
+        tracing::info!("client disconnected!");
+    }
+    .instrument(span)
+    .await
 }
